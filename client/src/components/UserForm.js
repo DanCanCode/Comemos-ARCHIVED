bad125bfed572e81278bbbce49aad62ceed78815
Daniel Espinal <dancancode@protonmail.com>
2022-09-07 05:26:08 -0400

    added show password toggle

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -9,21 +9,49 @@ import {
 } from "../styledcomps";
 
 const UserForm = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
   const [userData, setUserData] = useState({
     email: "",
     password: "",
   });
 
+  console.log(showPassword);
+
+  const handleSubmit = () => {};
   return (
     <Form>
       <InputField>
         <EmailIcon />
-        <Input type="text" name="email" placeholder="Email" />
+        <Input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={(e) =>
+            setUserData((prevState) => ({
+              ...prevState,
+              email: e.target.value,
+            }))
+          }
+        />
       </InputField>
 
       <InputField>
-        <LockIcon />
-        <Input type="text" name="password" placeholder="Password" />
+        <LockIcon
+          onClick={() =>
+            setShowPassword((prevShowPassword) => !prevShowPassword)
+          }
+        />
+        <Input
+          type={showPassword ? "text" : "password"}
+          name="password"
+          placeholder="Password"
+          onChange={(e) =>
+            setUserData((prevState) => ({
+              ...prevState,
+              password: e.target.value,
+            }))
+          }
+        />
       </InputField>
 
       <ButtonInput type="submit" value="Submit"></ButtonInput>
